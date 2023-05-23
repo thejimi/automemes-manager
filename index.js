1d0077cca349de67a9dc91c6bd30f60315bd47e2
thejimi <118121627+thejimi@users.noreply.github.com>
2023-05-23 17:24:13 +0200

    8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const allIntents = new Discord.Intents(7796);
 const client = new Discord.Client({ intents: allIntents });
-const { token } = require('./Configuration.json')
+const { token } = require('./config.json')
 
 client.on("ready", function(){
     console.log(`the client becomes ready to start`);

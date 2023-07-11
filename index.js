9fb40f698fa08383bcfde3d078e5de8382a8b69d
GoatInAHat <bennett.g.vernon@gmail.com>
2023-07-10 22:22:28 -0700

    idk

--- index.js
+++ index.js
@@ -85,13 +85,31 @@ client.on('messageCreate', async (message) => {
                                 .setAuthor({ name: "AutoMemes", iconURL: "https://cdn.discordapp.com/avatars/1009473107335061544/502ea7f2d245049bbffcd002bd1db626.webp?size=80", url: `https://discord.com/api/oauth2/authorize?client_id=1009473107335061544&permissions=533113203776&scope=applications.commands%20bot` })
                                 //.setDescription(`${message.embeds[0].fields[4].value} • Uploaded by [${message.embeds[0].fields[2].value}](https://discord.com/users/${message.embeds[0].fields[1].value})`)
                                 //.setDescription(`**${message.embeds[0].fields[4].value}**\n[Download](${message.embeds[0].fields[0].value}) • [AutoMemes](https://discord.com/api/oauth2/authorize?client_id=1009473107335061544&permissions=533113203776&scope=applications.commands%20bot) • [Meme World](https://discord.gg/eC5TUKVtPT)`)
-                                .setDescription(`**${message.embeds[0].fields[4].value}**\n[Download/Share](${message.embeds[0].fields[0].value}) • Uploaded by [${message.embeds[0].fields[2].value}](https://discord.com/users/${message.embeds[0].fields[1].value})`)
+                                .setDescription(`**${message.embeds[0].fields[4].value}**\n[Download/Share](${message.embeds[0].fields[0].value}) • Uploaded by [${message.embeds[0].fields[2].value}](https://discord.com/users/${message.embeds[0].fields[1].value}?${message.id})`)
                         ], files: [{attachment: buffer, name: `AutoMemes.${url.split('.').at(-1)}`}]});
                         await messagetopub.crosspost();
                         console.log(`Posted meme`)
                     });
                 });
                 break
+            }
+        }
+    }
+    if (message.channel.id == "1128172384709517394") {
+        for (channel_id of ids.video_channels) {
+            const channel = await client.channels.fetch(channel_id)
+            const messages = await channel.messages.fetch({ limit: 100 })
+            for (const mm of messages) {
+                for (const m of mm) {
+                    if (JSON.stringify(m).includes(message.content)) {
+                        console.log(m)
+                        channel.messages.fetch(m.id)
+                            .then(async msg => {
+                                console.log(await msg.delete())
+                                console.log("message deleted")
+                            })
+                    }
+                }
             }
         }
     }
